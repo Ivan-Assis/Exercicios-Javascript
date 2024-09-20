ce01cfd23f1647a2c6fb21a31af2281e318f30a6
Ivan Assis <119422265+Ivan-Assis@users.noreply.github.com>
2024-09-20 15:50:17 -0300

    15:50

---
+++ topico6.js
@@ -0,0 +1,14 @@
+//DOM - Document Object Model (DOM)
+// É uma interface que representa documentos HTML e XML através de objetos. Com ela é possível manipular a estrutura, estilo e conteúdo destes documentos.
+
+console.log(window);
+//WINDOW É O OBJETO GLOBAL DO BROWSER
+//POSSUI DIFERENTE MÉTODOS E PROPRIEDADES
+window.innerHeight; //retorna a altura do browser
+
+//Window e Document
+
+//São os objetos principais do DOM, boa parte da manipulação é feita através dos seus métodos e propriedades.
+
+window.alert('Isto é um alerta');
+alert('Isso é um alerta'); //funciona
