9d78215b41b7544c6e0355ad82224ddef48018e4
Ivan-Assis <ivan.assis766@gmail.com>
2024-09-21 18:12:14 -0300

    18:12

--- topico6.js
+++ topico6.js
@@ -10,36 +10,35 @@ window.innerHeight; //retorna a altura do browser
 
 //São os objetos principais do DOM, boa parte da manipulação é feita através dos seus métodos e propriedades.
 
-window.alert('Isto é um alerta');
-alert('Isso é um alerta'); //funciona
+window.alert("Isto é um alerta");
+alert("Isso é um alerta"); //funciona
 
-document.querySelector('h1'); //Seleciona o primeiro h1
+document.querySelector("h1"); //Seleciona o primeiro h1
 document.body; //retorna o body
-
 
 //Node
 
 //Toda tag html é representada pelo objeto Element e por isso herda os seus métodos e propriedades. Element é um tipo de objeto Node.
 
-const titulo = document.querySelector('h1');
+const titulo = document.querySelector("h1");
 
 titulo.innerText; //retorna o texto
 titulo.classList; //retorna as classes
 titulo.id; // retorna o id
 titulo.offsetHeight; //retorna a altura do elemento
-titulo.addEventListener('click', callback) //ativa a funcao de callback ao click no titulo
+titulo.addEventListener("click", callback); //ativa a funcao de callback ao click no titulo
 
 //ID
 
 //getElementById seleciona e retorna elementos do DOM
 
 //Seleciona pelo ID:
-const animaisSection = document.getElementById('animais');
-const contatoSection = document.getElementById('contato');
+const animaisSection = document.getElementById("animais");
+const contatoSection = document.getElementById("contato");
 
 //Retorna null caso não exista
 
-const naoExiste = document.getElementById('teste');
+const naoExiste = document.getElementById("teste");
 
 //Classe e Tag (AO VIVO)
 
@@ -47,12 +46,12 @@ const naoExiste = document.getElementById('teste');
 
 // Seleciona pela classe, retorna uma HTMLCollection
 
-const gridSection = document.getElementsByClassName('grid-section');
-const contato = document.getElementsByClassName('grid-section contato');
+const gridSection = document.getElementsByClassName("grid-section");
+const contato = document.getElementsByClassName("grid-section contato");
 
 // Seleciona todas as UL's, retorna uma HTMLCollection
 
-const ul = document.getElementsByTagName('ul');
+const ul = document.getElementsByTagName("ul");
 
 //Retorna o primeiro elemento:
 
@@ -62,24 +61,24 @@ console.log(gridSection[0]);
 
 //querySelector retorna o primeiro elemento que combinar com o seu seletor CSS.
 
-const animais = document.querySelector('.animais');
-const contato1 = document.querySelector('#contato1');
-const ultimoItem = documentl.querySelector('.animais-lista li:last-child');
+const animais = document.querySelector(".animais");
+const contato1 = document.querySelector("#contato1");
+const ultimoItem = documentl.querySelector(".animais-lista li:last-child");
 const linkCSS = document.querySelector('[href^="https://"]');
-const primeiroUl = document.querySelector('ul');
+const primeiroUl = document.querySelector("ul");
 
 //Busca dentro do Ul apenas
 
-const navItem = primeiroUl.querySelector('li');
+const navItem = primeiroUl.querySelector("li");
 
 //Seletor geral lista
 
 //querySelectorAll retorna todos os elementos compatíveis com o seletor CSS em uma NodeList
 
-const gridSection1 = document.querySelectorAll('.grid-section');
-const listas = document.querySelectorAll('ul');
-const titulos = document.querySelectorAll('.titulo');
-const fotosAnimais = document.querySelectorAll('.animais-lista img');
+const gridSection1 = document.querySelectorAll(".grid-section");
+const listas = document.querySelectorAll("ul");
+const titulos = document.querySelectorAll(".titulo");
+const fotosAnimais = document.querySelectorAll(".animais-lista img");
 
 //Retorna o segundo elemento
 console.log(gridSection1[1]);
@@ -88,10 +87,6 @@ console.log(gridSection1[1]);
 
 //A diferença está nos métodos e propriedades de ambas. Além disso a NodeList retornada com querySelectorAll é estática.
 
-const titulo1 = document.querySelectorAll('.titulo');
-const gridSectionHTML = document.getElementsByClassName('grid-section');
-const gridSectionNode2 = document.querySelectorAll('.grid-section')
-
-
-
-
+const titulo1 = document.querySelectorAll(".titulo");
+const gridSectionHTML = document.getElementsByClassName("grid-section");
+const gridSectionNode2 = document.querySelectorAll(".grid-section");
