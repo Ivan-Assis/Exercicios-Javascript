16d2e7ff4ea28edd5091edf91e9c898f6aa3f08f
Ivan-Assis <ivan.assis766@gmail.com>
2024-09-21 21:32:25 -0300

    21:32

---
+++ topico6.js
@@ -0,0 +1,60 @@
+//Retorne no console todas as imagens de um site
+
+const imagens = document.querySelectorAll("img");
+
+imagens.forEach(function (imagem) {
+  console.log("imagem.src");
+});
+
+//Selecione o primeiro h2
+
+const selecionaH2 = document.querySelector('h2');
+
+//Selecione o último p do site
+
+const ultimoP = document.querySelector('p:last-child');
+
+//Mostre no console cada parágrafo do site
+
+const paragrafo = document.querySelectorAll('p');
+
+paragrafo.forEach(function(prg){
+    console.log(prg);
+})
+
+//Como corrigir os erros abaixo?
+/*
+ERRADO:
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach(item, index => {
+    console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach( => {
+    console.log(i++);
+});
+
+imgs.forEach(() => i++);
+*/
+
+
+//CORRETO:
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach(function(item, index) {
+    console.log(item, index);
+});
+
+
+imgs.forEach( function() {
+    let i = 0;
+    console.log(i++);
+});
+
+
+imgs.forEach(() => console.log(i++));
+
